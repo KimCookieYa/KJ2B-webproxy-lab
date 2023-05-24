16302fe7bf76e18aa234f01be53db67aba1354d8
KimCookieYa <min49590@gmail.com>
2023-05-24 14:10:42 +0000

    fixup: make clean tiny

--- proxy.c
+++ proxy.c
@@ -10,7 +10,6 @@ void read_requesthdrs(rio_t *rp, void *buf, int serverfd, char *hostname, char *
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 void parse_uri(char *uri, char *hostname, char *port, char *path);
 
-static const int is_local_test = 1;
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
@@ -38,6 +37,8 @@ int main(int argc, char **argv) {
         *clientfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", client_hostname, client_port);
+        // if connection is accepted, create thread doit();
+        // main thread is ready for another request.
         Pthread_create(&tid, NULL, thread, clientfd);
     }
 }
@@ -58,6 +59,7 @@ void doit(int clientfd) {
     char *response_ptr, filename[MAXLINE], cgiargs[MAXLINE];
     rio_t request_rio, response_rio;
 
+    // client to proxy
     Rio_readinitb(&request_rio, clientfd);
     Rio_readlineb(&request_rio, request_buf, MAXLINE);
     printf("Request headers:\n %s\n", request_buf);
@@ -72,6 +74,7 @@ void doit(int clientfd) {
         return;
     }
 
+    // find cache
     web_object_t *cached_object = find_cache(path);
     if (cached_object) {
         send_cache(cached_object, clientfd);
@@ -79,15 +82,16 @@ void doit(int clientfd) {
         return;
     }
 
-    serverfd = is_local_test ? Open_clientfd(hostname, port) : Open_clientfd("52.79.234.188", port);
+    // proxy to server
+    serverfd = Open_clientfd(hostname, port);
     if (serverfd < 0) {
         clienterror(serverfd, method, "502", "Bad Gateway", "📍 Failed to establish connection with the end server");
         return;
     }
     Rio_writen(serverfd, request_buf, strlen(request_buf));
 
+    // server to proxy
     read_requesthdrs(&request_rio, request_buf, serverfd, hostname, port);
-
     Rio_readinitb(&response_rio, serverfd);
     while (strcmp(response_buf, "\r\n")) {
         Rio_readlineb(&response_rio, response_buf, MAXLINE);
@@ -95,11 +99,13 @@ void doit(int clientfd) {
             content_length = atoi(strchr(response_buf, ':') + 1);
         Rio_writen(clientfd, response_buf, strlen(response_buf));
     }
-
     response_ptr = malloc(content_length);
     Rio_readnb(&response_rio, response_ptr, content_length);
+
+    // proxy to client
     Rio_writen(clientfd, response_ptr, content_length);
 
+    // caching
     if (content_length <= MAX_OBJECT_SIZE) {
         web_object_t *web_object = (web_object_t *)calloc(1, sizeof(web_object_t));
         web_object->response_ptr = response_ptr;
@@ -145,10 +151,7 @@ void parse_uri(char *uri, char *hostname, char *port, char *path) {
         strncpy(port, port_ptr + 1, path_ptr - port_ptr - 1);
         strncpy(hostname, hostname_ptr, port_ptr - hostname_ptr);
     } else {
-        if (is_local_test)
-            strcpy(port, "80");
-        else
-            strcpy(port, "8000");
+        strcpy(port, "8000");
         strncpy(hostname, hostname_ptr, path_ptr - hostname_ptr);
     }
 }
@@ -187,8 +190,6 @@ void read_requesthdrs(rio_t *request_rio, void *request_buf, int serverfd, char 
         Rio_writen(serverfd, request_buf, strlen(request_buf));
     }
     if (!is_host_exist) {
-        if (!is_local_test)
-            hostname = "52.79.234.188";
         sprintf(request_buf, "Host: %s:%s\r\n", hostname, port);
         Rio_writen(serverfd, request_buf, strlen(request_buf));
     }
